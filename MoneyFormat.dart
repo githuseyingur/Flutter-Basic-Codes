593f627facc6931f0f35663579b8474ef8690073
Hüseyin Gür <120099096+githuseyingur@users.noreply.github.com>
2023-01-30 20:33:43 +0300

    Update MoneyFormat.dart

--- MoneyFormat.dart
+++ MoneyFormat.dart
@@ -1,4 +1,6 @@
-// Text(moneyFormat(price.toStringAsFixed(2)))
+// double price = 2981462.28;        
+// Text(moneyFormat(price.toStringAsFixed(2)))  --> 2,981,462.28       !!! price must be double --> toStringAsFixed(0) throws an ERROR !!!
+
 String moneyFormat(String price) {
     var value = price;
     if (price.length > 2) {
